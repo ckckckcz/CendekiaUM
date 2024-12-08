ed92974fd8525beca5503171f06990b7f180bdf1
Vidi Joshubzky Saviola <103508462+TMTMPST@users.noreply.github.com>
2024-12-08 21:00:10 +0700

    asd

--- src/views/App.js
+++ src/views/App.js
@@ -1,9 +1,11 @@
 import Navbar from "../components/Navbar";
+import Tujuan from "../components/Tujuan";
 
 export default function App() {
     return(
         <>
             <Navbar />
+            <Tujuan />
         </>
     );
 }
