c0ce870bffdc957289feaf8b08b806fada26daf7
ckckckcz <rriovld@gmail.com>
2024-12-08 20:58:06 +0700

    update: logo

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,11 +1,14 @@
+import Logo from "../assets/img/logo.png"
+import LogoUM from "../assets/img/logoUM.png"
 export default function Navbar() {
     return(
         <>
         <nav class="bg-transparent">
             <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-6">
-                <a href="https://flowbite.com/" class="flex items-center space-x-3 rtl:space-x-reverse">
-                    <img src="https://flowbite.com/docs/images/logo.svg" class="h-8" alt="Flowbite Logo" />
-                    <span class="self-center text-2xl font-semibold whitespace-nowrap ">Flowbite</span>
+                <a href="javascript:void(0" class="flex items-center space-x-3 rtl:space-x-reverse">
+                    <img src={Logo} class="h-8" alt="Cendekia Logo" />
+                    <img src={LogoUM} class="h-8" alt="Cendekia Logo" />
+                    <span class="self-center text-2xl font-semibold whitespace-nowrap ">Cendekia UM</span>
                 </a>
                 <div class="flex md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
                     <button type="button" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center">Get started</button>
