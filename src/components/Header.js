74721ac2f7bf05cb09ce465bd78262ae3e7c19eb
ckckckcz <rriovld@gmail.com>
2024-12-09 06:00:42 +0700

    update: h for header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,23 +1,33 @@
 import Avatar from "./Header/Avatar.js";
 
 export default function Header() {
-    return(
+    return (
         <>
-        <section class="bg-transparent mt-4 lg:mt-14">
-            <div class="py-8 px-4 mx-auto max-w-screen-xl text-center lg:py-16">
-                <Avatar />
-                <h1 class="mb-4 text-4xl font-extrabold tracking-tight leading-none text-gray-900 md:text-5xl lg:text-6xl lg:text-center text-left md:text-left">Belajar Secara Mandiri, Berkembang Secara Bersama</h1>
-                <p class="mb-8 text-lg font-normal text-gray-500 lg:text-xl lg:text-center text-left md:text-left">Pendekatan pendidikan yang mengutamakan kebebasan belajar secara individu, sambil tetap memberikan dukungan dan kolaborasi dengan sesama pelajar.</p>
-                <div class="flex flex-col space-y-4 sm:flex-row sm:justify-center sm:space-y-0">
-                    <a href="#" class="inline-flex justify-center items-center py-3 px-5 text-base font-medium text-center text-white rounded-lg bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300">
+            <section   section className="bg-transparent lg:h-[910px] flex items-center justify-center mt-4">
+                <div className="py-8 px-4 mx-auto max-w-screen-xl lg:text-center text-left">
+                    <Avatar />
+                    <h1 className="mb-4 text-4xl font-extrabold tracking-tight leading-none text-gray-900 md:text-5xl lg:text-6xl">
+                    Belajar Secara Mandiri, Berkembang Secara Bersama
+                    </h1>
+                    <p className="mb-8 text-lg font-normal text-gray-500 lg:text-xl">
+                    Pendekatan pendidikan yang mengutamakan kebebasan belajar secara individu, sambil tetap memberikan dukungan dan kolaborasi dengan sesama pelajar.
+                    </p>
+                    <div className="flex flex-col space-y-4 sm:flex-row sm:justify-center sm:space-y-0">
+                    <a
+                        href="#"
+                        className="inline-flex justify-center items-center py-3 px-5 text-base font-medium text-center text-white rounded-lg bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300"
+                    >
                         Ayo Belajar !
                     </a>
-                    <a href="#" class="py-3 px-5 sm:ms-4 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100 ">
+                    <a
+                        href="#"
+                        className="py-3 px-5 sm:ms-4 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100"
+                    >
                         Panduan
-                    </a>  
+                    </a>
+                    </div>
                 </div>
-            </div>
-        </section>
+            </section>
         </>
     );
-}
+  }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,4 @@
 import Logo from "../assets/img/logo.png"
-import LogoUM from "../assets/img/logoUM.png"
 export default function Navbar() {
     return(
         <>
@@ -7,11 +6,10 @@ export default function Navbar() {
             <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-6">
                 <a href="javascript:void(0" class="flex items-center space-x-3 rtl:space-x-reverse">
                     <img src={Logo} class="h-8" alt="Cendekia Logo" />
-                    <img src={LogoUM} class="h-8" alt="Cendekia Logo" />
                     <span class="self-center text-2xl font-semibold whitespace-nowrap ">Cendekia UM</span>
                 </a>
                 <div class="flex md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
-                    <button type="button" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center">Get started</button>
+                    <button type="button" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center lg:block hidden">Get started</button>
                     <button data-collapse-toggle="navbar-cta" type="button" class="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 " aria-controls="navbar-cta" aria-expanded="false">
                         <span class="sr-only">Open main menu</span>
                         <svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
