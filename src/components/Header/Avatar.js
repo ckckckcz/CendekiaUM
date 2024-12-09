bcb9a2734d043955030c6ec5a25ba471df894d08
ckckckcz <rriovld@gmail.com>
2024-12-09 14:23:18 +0700

    update: avatar title

--- src/components/Header/Avatar.js
+++ src/components/Header/Avatar.js
@@ -1,10 +1,18 @@
+import Testimoni1 from "../../assets/img/testimoni/testimoni1.png"
+import Testimoni2 from "../../assets/img/testimoni/testimoni2.png"
+import Testimoni3 from "../../assets/img/testimoni/testimoni3.png"
 export default function Avatar(){
     return(
-        <div class="flex -space-x-4 rtl:space-x-reverse lg:justify-center justify-left mb-4">
-            <img class="w-10 h-10 border-2 border-white rounded-full dark:border-gray-800" src="/docs/images/people/profile-picture-5.jpg" alt="" />
-            <img class="w-10 h-10 border-2 border-white rounded-full dark:border-gray-800" src="/docs/images/people/profile-picture-2.jpg" alt=""/>
-            <img class="w-10 h-10 border-2 border-white rounded-full dark:border-gray-800" src="/docs/images/people/profile-picture-3.jpg" alt=""/>
-            <a class="flex items-center justify-center w-10 h-10 text-xs font-medium text-white bg-gray-700 border-2 border-white rounded-full hover:bg-gray-600 dark:border-gray-800" href="#">+99</a>
-        </div>
+        <>
+            <div class="flex lg:justify-center justify-left gap-3 items-center mb-4">
+                <div class="flex -space-x-4 rtl:space-x-reverse lg:justify-center justify-left">
+                    <img class="w-10 h-10 border-2 border-white rounded-full object-cover" src={Testimoni1} alt="" />
+                    <img class="w-10 h-10 border-2 border-white rounded-full object-cover" src={Testimoni2} alt="" />
+                    <img class="w-10 h-10 border-2 border-white rounded-full object-cover" src={Testimoni3} alt="" />
+                    <a class="flex items-center justify-center w-10 h-10 text-xs font-medium text-white bg-blue-700 border-2 border-white rounded-full hover:bg-blue-800"href="#">+99</a>
+                </div>
+                <p class="font-medium text-lg lg:block hidden">Pengguna Produktif!!</p>
+            </div>
+        </>
     );
 }
