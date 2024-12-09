3c68fe70b5ab44835fe6508cedce27f53d5d36bf
ckckckcz <rriovld@gmail.com>
2024-12-09 07:05:28 +0700

    update: Header text

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,12 +4,12 @@ export default function Header() {
     return (
         <>
             <section   section className="bg-transparent lg:h-[910px] flex items-center justify-center mt-4">
-                <div className="py-8 px-4 mx-auto max-w-screen-xl lg:text-center text-left">
+                <div className="py-8 px-4 mx-auto max-w-screen-xl">
                     <Avatar />
-                    <h1 className="mb-4 text-4xl font-extrabold tracking-tight leading-none text-gray-900 md:text-5xl lg:text-6xl">
+                    <h1 className="mb-4 text-4xl font-extrabold tracking-tight leading-none text-gray-900 md:text-5xl lg:text-6xl lg:text-center text-left">
                     Belajar Secara Mandiri, Berkembang Secara Bersama
                     </h1>
-                    <p className="mb-8 text-lg font-normal text-gray-500 lg:text-xl">
+                    <p className="mb-8 text-lg font-normal text-gray-500 lg:text-xl lg:text-center text-left">
                     Pendekatan pendidikan yang mengutamakan kebebasan belajar secara individu, sambil tetap memberikan dukungan dan kolaborasi dengan sesama pelajar.
                     </p>
                     <div className="flex flex-col space-y-4 sm:flex-row sm:justify-center sm:space-y-0">
