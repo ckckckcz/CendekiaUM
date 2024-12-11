5cb18de927c7d54f2230169d38223613533226cd
ckckckcz <rriovld@gmail.com>
2024-12-11 11:28:30 +0700

    update: footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -8,7 +8,7 @@ export default function Footer() {
                     <img src={Logo} class="h-8" alt="Cendekia UM Logo" />
                     <span class="self-center text-2xl font-semibold whitespace-nowrap ">Cendekia UM</span>
                 </a>
-                <ul class="flex flex-wrap items-center mb-6 text-sm font-medium text-gray-500 sm:mb-0 ">
+                {/* <ul class="flex flex-wrap items-center mb-6 text-sm font-medium text-gray-500 sm:mb-0 ">
                     <li>
                         <a href="#" class="hover:underline me-4 md:me-6">Riovaldo</a>
                     </li>
@@ -18,7 +18,7 @@ export default function Footer() {
                     <li>
                         <a href="#" class="hover:underline me-4 md:me-6">Rijal Abr</a>
                     </li>
-                </ul>
+                </ul> */}
             </div>
             <hr class="my-6 border-gray-200 sm:mx-auto lg:my-8" />
             <span class="block text-sm text-gray-500 sm:text-center ">© 2024 <a href="https://flowbite.com/" class="hover:underline">Cendekia™</a>. All Rights Reserved.</span>
